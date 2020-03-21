b80fd333dc54b880f624c2af0c7a0bde360c7a68
Catherine Diaz <cadia@microsoft.com>
2020-03-20 17:33:40 -0700

    Code clean up

--- Assets/MarqueeSelection/Scripts/MarqueeSelection.cs
+++ Assets/MarqueeSelection/Scripts/MarqueeSelection.cs
@@ -33,51 +33,48 @@ public class MarqueeSelection : MonoBehaviour,  IMixedRealityHandJointHandler
         set => onObjectSelected = value;
     }
 
-    //float maxDistance;
-    float speed;
-    bool hitDetect;
-
-    protected Collider marqueeAreaCollider;
+    // BoxCast hit
     protected RaycastHit hit;
 
-    //private GameObject marquee = null;
-    private Vector3 marqueeStartPosition;
+    bool hitDetect;
 
+    // Corner points of the marquee
+    private Vector3 marqueeStartPosition;
     private Vector3 marqueeCenter;
+    private Vector3 topRight;
+    private Vector3 bottomLeft;
 
     private LineRenderer lineRenderer;
 
     private Vector3 boxHalfVolume;
 
-    private Vector3 topRight;
-
-    private Vector3 bottomLeft;
+    private Vector3[] marqueeCornerPositions = new Vector3[5];
 
     public Vector3 CurrentThumbPosition
     {
         get
         {
-            MixedRealityPose thumb;
-            HandJointUtils.TryGetJointPose(TrackedHandJoint.ThumbTip, Handedness.Right, out thumb);
+            HandJointUtils.TryGetJointPose(TrackedHandJoint.ThumbTip, Handedness.Right, out MixedRealityPose thumb);
             return thumb.Position;
         }
     }
 
+    void Start()
+    {
+        CoreServices.InputSystem?.RegisterHandler<IMixedRealityHandJointHandler>(this);
+    }
+
+    /// <summary>
+    /// Checks if the hand is in the thumbs up gesture by measuring distances between joints
+    /// </summary>
     private bool ThumbsUp()
     {
-        MixedRealityPose indexTip;
-        MixedRealityPose middleTip;
-        MixedRealityPose ringTip;
-        MixedRealityPose pinkyTip;
-        MixedRealityPose palm;
-        MixedRealityPose thumbTip;
-
-        HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out indexTip);
-        HandJointUtils.TryGetJointPose(TrackedHandJoint.MiddleTip, Handedness.Right, out middleTip);
-        HandJointUtils.TryGetJointPose(TrackedHandJoint.RingTip, Handedness.Right, out ringTip);
-        HandJointUtils.TryGetJointPose(TrackedHandJoint.PinkyTip, Handedness.Right, out pinkyTip);
-        HandJointUtils.TryGetJointPose(TrackedHandJoint.Palm, Handedness.Right, out palm);
-        HandJointUtils.TryGetJointPose(TrackedHandJoint.ThumbTip, Handedness.Right, out thumbTip);
+        HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out MixedRealityPose indexTip);
+        HandJointUtils.TryGetJointPose(TrackedHandJoint.MiddleTip, Handedness.Right, out MixedRealityPose middleTip);
+        HandJointUtils.TryGetJointPose(TrackedHandJoint.RingTip, Handedness.Right, out MixedRealityPose ringTip);
+        HandJointUtils.TryGetJointPose(TrackedHandJoint.PinkyTip, Handedness.Right, out MixedRealityPose pinkyTip);
+        HandJointUtils.TryGetJointPose(TrackedHandJoint.Palm, Handedness.Right, out MixedRealityPose palm);
+        HandJointUtils.TryGetJointPose(TrackedHandJoint.ThumbTip, Handedness.Right, out MixedRealityPose thumbTip);
 
         float indexPalmDistance = Vector3.Distance(palm.Position, indexTip.Position);
         float middlePalmDistance = Vector3.Distance(palm.Position, middleTip.Position);
@@ -85,21 +82,21 @@ public class MarqueeSelection : MonoBehaviour,  IMixedRealityHandJointHandler
         float pinkyPalmDistance = Vector3.Distance(palm.Position, pinkyTip.Position);
         float thumbPalmDistance = Vector3.Distance(palm.Position, thumbTip.Position);
 
-
-        if (indexPalmDistance < 0.06f && middlePalmDistance < 0.06f && ringPalmDistance < 0.06f && pinkyPalmDistance < 0.06f && thumbPalmDistance > 0.1f)
+        if (indexPalmDistance < 0.08f && middlePalmDistance < 0.08f && ringPalmDistance < 0.08f && pinkyPalmDistance < 0.08f && thumbPalmDistance > 0.1f)
         {
             return true;
         }
+
         return false;
     }
 
     public void OnHandJointsUpdated(InputEventData<IDictionary<TrackedHandJoint, MixedRealityPose>> eventData)
     {
+        // If the hand is in the thumbs up gesture
         if (IsThumbsUp)
         {
             if (!isSelectionDrawing)
             {
-                
                 StartSelectionDraw();                
             }
             else
@@ -116,22 +113,23 @@ public class MarqueeSelection : MonoBehaviour,  IMixedRealityHandJointHandler
         }
     }
 
-    void Start()
-    {
-        CoreServices.InputSystem?.RegisterHandler<IMixedRealityHandJointHandler>(this);
 
-        speed = 20.0f;
-    }
-
+    /// <summary>
+    /// Get enable the line renderer attached to the game object
+    /// </summary>
     private void StartSelectionDraw()
     {
+        // Get the line renderer
         lineRenderer = gameObject.GetComponent<LineRenderer>();
         lineRenderer.enabled = true;
 
+        // The position count is 5 starting at the left top corner, top right corner, bottom right, bottom left
+        // then the start position again 
         lineRenderer.positionCount = 5;
 
         marqueeStartPosition = CurrentThumbPosition;
 
+        // Add starting position to linerenderer
         lineRenderer.SetPosition(0, marqueeStartPosition);
 
         isSelectionDrawing = true;
@@ -139,27 +137,28 @@ public class MarqueeSelection : MonoBehaviour,  IMixedRealityHandJointHandler
 
     private void UpdateSelectionDraw()
     {
+        // Calculate top right and bottom left positions based on the start position and current position 
         topRight = CurrentThumbPosition + (Vector3.up * (marqueeStartPosition.y - CurrentThumbPosition.y));
-
         bottomLeft = marqueeStartPosition + (Vector3.down * (marqueeStartPosition.y - CurrentThumbPosition.y));
 
+        // Add points to line renderer
         lineRenderer.SetPosition(1, topRight);
-
         lineRenderer.SetPosition(2, CurrentThumbPosition);
-
         lineRenderer.SetPosition(3, bottomLeft);
-
         lineRenderer.SetPosition(4, marqueeStartPosition);
 
+        // Calculate center of marquee for the box cast
         marqueeCenter = (marqueeStartPosition + CurrentThumbPosition) / 2;
 
+        // Calculate half volume of the box based on the marquee size 
         float xExtents = Mathf.Abs(marqueeCenter.x - CurrentThumbPosition.x);
         float yExtents = Mathf.Abs(marqueeCenter.y - marqueeStartPosition.y);
         float zExtents = 0.5f;
 
+        // For the box cast
         boxHalfVolume = new Vector3(xExtents, yExtents, zExtents);
 
-        StartBoxCast(marqueeCenter, boxHalfVolume);
+        PerformBoxCast(marqueeCenter, boxHalfVolume);
     }
 
     private void StopSelectionDraw()
@@ -169,25 +168,19 @@ public class MarqueeSelection : MonoBehaviour,  IMixedRealityHandJointHandler
         isObjectSelected = false;
     }
 
-    private void StartBoxCast(Vector3 marqueeCenter, Vector3 halfScaleOfBox)
+    private void PerformBoxCast(Vector3 marqueeCenter, Vector3 halfScaleOfBox)
     {
-        //Simple movement in x and z axes
-        float xAxis = Input.GetAxis("Horizontal") * speed;
-        float zAxis = Input.GetAxis("Vertical") * speed;
-        transform.Translate(new Vector3(xAxis, 0, zAxis));
-
         hitDetect = Physics.BoxCast(marqueeCenter, halfScaleOfBox, CameraCache.Main.transform.forward, out hit);
 
         if (hitDetect)
         {
-            //Output the name of the Collider your Box hit
-            SetSelectedState(hit.collider.gameObject);
-            Debug.Log("Hit : " + hit.collider.name);
+            SelectObject(hit.collider.gameObject);
         }
     }
 
-    private void SetSelectedState(GameObject go)
+    private void SelectObject(GameObject go)
     {
+        // Create event that passes a game object data instead of this 
         Material mat = go.GetComponent<MeshRenderer>().material;
         mat.color = Color.yellow;
         var tapToPlace = go.AddComponent<TapToPlace>();
